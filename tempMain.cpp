19ef65345e44259f15bdbf7501b706f15e879633
quytm_58 <quytm_58@vnu.edu.vn>
2014-11-22 16:59:33 +0700

    quy tm :  main

---
+++ tempMain.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <stdio.h>
+#include <cstring>
+#include <cmath>
+using namespace std;
+#include "readFile.cpp"
+//#include "Data.cpp"
+
+int main()
+{
+	for (int i=0; i<SIZE; i++){
+		dic[i] = new Data();
+	}
+	Data * data= new Data("love","yeu say dam");
+	readFile();
+	Data* r = find("love");
+	if (r==NULL){
+		writeFile(data);
+		insert(data);
+	}
+	
+
+	for (int i=0; i<SIZE; i++){
+		if (dic[i]->key!=0)
+		cout << dic[i]->word << endl;
+	}
+	Data* result = find("love");
+	if (result==NULL)cout << "deo co";
+	else cout << endl << "nghia cua tu la: " << result->mean << endl;
+	return 0;
+}
