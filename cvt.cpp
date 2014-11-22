01e9b1d38de4b85dc397d39daeca29c59bb4a5db
tutv_58 <tutv95@gmail.com>
2014-11-22 11:14:50 +0700

    fix cvt

--- cvt.cpp
+++ cvt.cpp
@@ -1,12 +1,9 @@
-#include <iostream>
-#include <cmath>
 using namespace std;
 long cvt(string x) {
 	long temp = 0;
 	for (int i=x.length()-1; i>=0; i--) {
 		int num = (int)(x.at(i) - 96);
 		temp += (long)(num * pow(25, i));
-		// temp += num;
 	}
 	return temp;
 }
