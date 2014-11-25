b8fa118b1a12e74e48f9264328d073a456e53698
tutv_58 <tutv95@gmail.com>
2014-11-25 17:35:26 +0700

    tutv95: Add test find()

--- tempMain.cpp
+++ tempMain.cpp
@@ -9,25 +9,35 @@
 using namespace std;
 
 int main() {
+
+	//Load file
 	Dictionary av = Dictionary();
 	av.loadFile("Data.txt");
 	av.print();
-
 	cout << "-------------------------------------\n";
+
+	//Remove Word
 	bool del = av.remove("bad");
 	if (del) cout << "Xoa thanh cmn cong :]]]]]]\n";
 	else cout << "Eo xoa duoc :[[\n";
 
 	av.print();
 	av.updateFile("Data.txt");
-
 	cout << "-------------------------------------\n";
 
+	//Insert Word
 	Data * wordDic = new Data("dictionary", "Tu dien");
 
 	av.insert(wordDic);
 	av.updateFile("Data.txt");
 
 	av.print();
+	cout << "-------------------------------------\n";
+
+	//Find Word
+	Data * findWord = av.find("love");
+	if (findWord) cout << "love  ------------>  " << findWord->mean << endl;
+	else cout << "Eo co tu day!!!" << endl;
+
 	return 0;
 }
